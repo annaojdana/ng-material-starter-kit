6e9bd0d7a6a9a69a5411e2fda97deddc4f77c876
Anna <anna.ojdana@gmail.com>
2022-11-16 13:44:00 +0100

    feat: checkbox of categories implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { CategorySelectComponent } from './components/category-select/category-select.component';
 import { CryptoChipsListComponent } from './components/crypto-chips-list/crypto-chips-list.component';
 import { PublicHolidaysComponent } from './components/public-holidays/public-holidays.component';
+import { CategoryCheckboxComponent } from './components/category-checkbox/category-checkbox.component';
 import { ProductListComponentModule } from './components/product-list/product-list.component-module';
 import { ProductsServiceModule } from './services/products.service-module';
 import { CategorySelectComponentModule } from './components/category-select/category-select.component-module';
@@ -12,13 +13,11 @@ import { CryptoChipsListComponentModule } from './components/crypto-chips-list/c
 import { CryptosServiceModule } from './services/cryptos.service-module';
 import { PublicHolidaysComponentModule } from './components/public-holidays/public-holidays.component-module';
 import { HolidaysServiceModule } from './services/holidays.service-module';
+import { CategoryCheckboxComponentModule } from './components/category-checkbox/category-checkbox.component-module';
 
 @NgModule({
   imports: [RouterModule.forRoot(
-    [{ path: 'products', component: ProductListComponent },
-      { path: 'categories', component: CategorySelectComponent },
-      { path: 'crypto', component: CryptoChipsListComponent },
-      { path: 'public-holidays', component: PublicHolidaysComponent }]), ProductListComponentModule, ProductsServiceModule, CategorySelectComponentModule, CategoryServiceModule, CryptoChipsListComponentModule, CryptosServiceModule, PublicHolidaysComponentModule, HolidaysServiceModule],
+    [{ path: 'products', component: ProductListComponent }, { path: 'categories', component: CategorySelectComponent }, { path: 'crypto', component: CryptoChipsListComponent }, { path: 'public-holidays', component: PublicHolidaysComponent }, { path: 'checkbox-categories', component: CategoryCheckboxComponent }]), ProductListComponentModule, ProductsServiceModule, CategorySelectComponentModule, CategoryServiceModule, CryptoChipsListComponentModule, CryptosServiceModule, PublicHolidaysComponentModule, HolidaysServiceModule, CategoryCheckboxComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/components/category-checkbox/category-checkbox.component.ts
@@ -0,0 +1,18 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProductModel } from '../../models/product.model';
+import { CategoryService } from '../../services/category.service';
+
+@Component({
+  selector: 'app-category-checkbox',
+  styleUrls: ['./category-checkbox.component.scss'],
+  templateUrl: './category-checkbox.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CategoryCheckboxComponent {
+  readonly list$: Observable<ProductModel[]> = this._categoryService.getAll();
+
+  constructor(private _categoryService: CategoryService) {
+  }
+}
